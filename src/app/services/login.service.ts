cdf37948923a74d6cfe906177a1c6c8d94c50e0a
erwin <erwin@experd.com>
2018-09-20 11:58:05 +0700

    change ui

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Rx';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as globalVar from '../global';
-import { StatemanagementService } from '../services/statemanagement.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,20 +10,17 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class LoginService {
-  private url = globalVar.global_um + '/users';  // URL to web api
-
-  constructor(private httpClient: HttpClient, private stateService: StatemanagementService) {
+  private url = '/users';  // URL to web api
+  constructor(private httpClient: HttpClient) {
   }
 
   login(username, password): Observable<any> {
-    return this.httpClient.post(this.url + '/login', { username: username, password: password, appcode:globalVar.global_appcode }, httpOptions)
-      .map(
+    return this.httpClient.post(this.url + '/login', { username: username, password: password }, httpOptions)
+    .pipe(map(
         res => {
           localStorage.setItem('currentUser', JSON.stringify(res));
-          this.stateService.setCurrentStateLogin("1");
           return res;
-        }
-      );
+        }));
   }
 
   chPwd(username: string, password: string): Observable<any> {
@@ -35,17 +30,15 @@ export class LoginService {
     const headers = _headers.append('x-access-token', token.token);
 
     return this.httpClient.post(this.url + '/changepwd', { username: username, password: password }, { headers: headers })
-      .map(
+      .pipe(map(
         res => {
           return res;
-        }
-      );
+        }));
   }
 
   logout() {
     setTimeout(() => {
       localStorage.clear();
-      this.stateService.setCurrentStateLogin("0");
     }, 500);
   }
 }
